0664da39fcb4924ee6b38f1b4d955cff1a429930
allisson-faiad <allisson.faiad001@gmail.com>
2021-02-10 23:18:16 -0300

    Netflix Finalizado, Video de Fundo Header Funcionando

--- Recriando a Interface do Netflix/js/setup.js
+++ Recriando a Interface do Netflix/js/setup.js
@@ -1,16 +1,47 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:5
+        1000: {
+            items: 5
         }
     }
 })
+var test = 0;
+$(".filme-principal").mouseover(
+    function() {        
+        $('.filme-principal #video')[0].play(); // Iniciar visualização do video
+        $('.filme-principal').css({
+            height: '80vh',
+            transition: '1s ease-in-out'
+        }); 
+        $('.filme-principal .titulo').css({
+            'margin-top': '40%',
+            opacity: '0',
+            transition: '1s ease'
+        });  
+        $('.filme-principal .descricao').css({
+            opacity: '0',
+            transition: '1s ease'
+        });  
+        $('.filme-principal .botao').css({
+            opacity: '0',
+            transition: '1s ease'
+        });  
+    }
+
+);
+// $(".filme-principal").mouseout(
+//     function() {
+//         var teste = $('.filme-principal #video')[0].currentTime;
+//         $('.filme-principal #video')[0].pause(); // Iniciar visualização do video
+//         $('.filme-principal #video')[0].load();
+//     }
+// );
